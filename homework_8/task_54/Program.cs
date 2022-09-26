ae4d4034cc8eb4ddb444fbe5e57df28360cb8322
Anna <a.chetkina.a@yandex.ru>
2022-09-26 22:49:48 +0300

    add homework_8

---
+++ homework_8/task_54/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] FillArray(int n, int m)
+{
+    int[,] mas = new int[n, m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++) mas[i, j] = new Random().Next(1, 10);
+    }
+    return mas;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j != arr.GetLength(1) - 1) Console.Write($"{arr[i, j]}, ");
+            else if (i == arr.GetLength(0) - 1 && j == arr.GetLength(1) - 1) Console.WriteLine($"{arr[i, j]}");
+            else if (j == arr.GetLength(1) - 1) Console.WriteLine($"{arr[i, j]},");
+        }
+    }
+}
+
+void SelectionSortMax(int[,] array){
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++) {
+            int maxPos = j;
+            for (int k = j + 1; k < array.GetLength(1); k++){
+                if (array[i, k] > array[i, maxPos]) maxPos = k;
+            }
+            (array[i, j], array[i, maxPos]) = (array[i, maxPos], array[i, j]);
+
+        }
+    }
+}
+
+int[,] arr = FillArray(4, 4);
+// int[,] arr = new int[,] 
+// {
+//     {1, 4, 7, 2},
+//     {5, 9, 2, 3},
+//     {8, 4, 2, 4}
+// };
+PrintArray(arr);
+System.Console.WriteLine();
+SelectionSortMax(arr);
+PrintArray(arr);
---
+++ homework_8/task_62/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int[,] FillArray(int n)
+{
+    int initialVal = 1;
+    int startCol = 0;
+    int endCol = n - 1;
+    int startRow = 0;
+    int endRow = n - 1;
+    int[,] arr = new int[n, n];
+    while (startCol <= endCol && startRow <= endRow) {
+        
+        for (int i = startCol; i <= endCol; i++) 
+        { 
+            arr[startRow, i] = initialVal;
+            initialVal++;
+        }
+        startRow++;
+
+        for (int j = startRow; j <= endRow; j++) 
+        { 
+            arr[j, endCol] = initialVal;
+            initialVal++;
+        }
+        endCol--;
+
+        for (int k = endCol; k >= startCol; k--) 
+        { 
+            arr[endRow, k] = initialVal;
+            initialVal++;
+        }
+        endRow--;
+
+        for (int m = endRow; m >= startRow; m--) 
+        { 
+            arr[m, startCol] = initialVal;
+            initialVal++;
+        }
+        startCol++;
+    }
+
+    
+        
+    return arr;
+
+    
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j != arr.GetLength(1) - 1) Console.Write($"{arr[i, j]}, ");
+            else if (i == arr.GetLength(0) - 1 && j == arr.GetLength(1) - 1) Console.WriteLine($"{arr[i, j]}");
+            else if (j == arr.GetLength(1) - 1) Console.WriteLine($"{arr[i, j]},");
+        }
+    }
+}
+
+int[,] arr = FillArray(4);
+PrintArray(arr);
+System.Console.WriteLine();
+int[,] arr1 = FillArray(6);
+PrintArray(arr1);
+
